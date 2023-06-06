288fbc2059dee9c9d9cf33a2d408f6c31bede7eb
Fexean <3699814-Fexean@users.noreply.gitlab.com>
2023-06-06 18:27:14 +0300

    nscr support

---
+++ src/tests/nscr_test.py
@@ -0,0 +1,35 @@
+
+import unittest
+from ntr.nscr import NSCR, MapEntry
+
+
+class TestNscr(unittest.TestCase):
+
+    def test_creation(self):
+        x = NSCR(256,128)
+        self.assertEqual(x.width, 256)
+        self.assertEqual(x.height, 128)
+
+
+    def test_pack_preserves_dim(self):
+        x = NSCR(256, 128)
+        y = x.pack()
+        z = NSCR.unpack(y)
+        self.assertEqual(x.width, z.width)
+        self.assertEqual(x.height, z.height)
+        self.assertEqual(len(x.map), len(z.map))
+    
+    def test_pack_preserves_data(self):
+        x = NSCR(256, 128)
+        y = x.pack()
+        z = NSCR.unpack(y)
+        self.assertEqual(x, z)
+    
+    def test_pack_preserves_edited_data(self):
+        x = NSCR(256, 128)
+        x.set_entry(2, 5, MapEntry(43))
+
+        y = x.pack()
+        z = NSCR.unpack(y)
+        self.assertEqual(x, z)
+
